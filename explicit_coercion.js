1542122250429e094af5c91f9f2a9fbbf6ba016a
RanjithKumar <ranjithkumarpatil@gmail.com>
2022-08-16 23:06:11 +0530

    coercion types

---
+++ explicit_coercion.js
@@ -0,0 +1,8 @@
+var x = 6;
+var y = "6";
+var z = Number(y);
+if (x === z) {
+  console.log("both are equal");
+} else {
+  console.log("Not equal");
+}
---
+++ implicit_coercion.js
@@ -0,0 +1,7 @@
+var x = 6;
+var y = "6";
+if (x == y) {
+  console.log("both are equal");
+} else {
+  console.log("Not equal");
+}
